dbadf343d284fb7f9919aea8601988437fd2fb33
Mohamed Saeed <mohamedsaed51@gmail.com>
2026-08-02 18:21:31 +0300

    Update APPS_SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 window.ARRIVE_CONFIG = {
   // Paste the Apps Script Web App URL for THIS dashboard's sheet.
   // See SETUP_GUIDE.md for how to generate it.
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzaQWbPqwYPBH4lhTbS33n6EpNJClXAJ69_XzfNdpFgy8DehAGkBjZO1jznlw3lqoXbVA/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxc2tsanxMs3kCuzla6FH6lmMcqbdvJrzXsvRs__iOVsZoAROvDsvQbOQSLh1wupD4tTA/exec',
 
   // Auto-refresh interval in minutes. Set to 0 to disable.
   AUTO_REFRESH_MINUTES: 5,
